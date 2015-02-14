903ba65c2ebf6fd364714fe9f9bec20286f1c755
Roken111 <roken_mail@gmx.de>
2015-02-14 13:44:04 +0100

    Es kappt nicht :/

--- LootDistributionTracker.lua
+++ LootDistributionTracker.lua
@@ -6,38 +6,38 @@ local frame = CreateFrame("FRAME", "FooAddonFrame");
 frame:RegisterEvent("CHAT_MSG_LOOT");
 local function eventHandler(...)
 
-	if LOOTS == nil then
-    	LOOTS = {};
+  if LOOTS == nil then
+      LOOTS = {};
     end
 
 
-	local args = {...};
-	local msg = args[3];
+  local args = {...};
+  local msg = args[3];
 
-	if string.match(msg, "Beute") or string.match(msg, "Loot") then
-		local i, j = string.find(msg, "%[.*%]");
-		local item = string.sub(msg, i+1, j-1);
-	--	printTable( mysplit(msg, "%S"));
-	
-		-- zieht hier das erste Wort aus dem Chat ("Ihr" erhaltet ..., "Hummeel" erhält ... )
-		local player = mysplit(msg, "%s")[1];
-		
+  if string.match(msg, "Beute") or string.match(msg, "Loot") then
+    local i, j = string.find(msg, "%[.*%]");
+    local item = string.sub(msg, i+1, j-1);
+  --  printTable( mysplit(msg, "%S"));
+  
+    -- zieht hier das erste Wort aus dem Chat ("Ihr" erhaltet ..., "Hummeel" erhält ... )
+    local player = mysplit(msg, "%s")[1];
+    
     -- aus "Ihr" wird "Ralphus" oder "Hummeel" :)
-		if string.match(player, "Ihr") or string.match(player, "You") then
-			player = UnitName("player");	
-		end
+    if string.match(player, "Ihr") or string.match(player, "You") then
+      player = UnitName("player");  
+    end
 
-		local name, type, difficultyIndex, difficultyName, maxPlayers, dynamicDifficulty,
-    	isDynamic, mapID = GetInstanceInfo();
+    local name, type, difficultyIndex, difficultyName, maxPlayers, dynamicDifficulty,
+      isDynamic, mapID = GetInstanceInfo();
 
-    	table.insert(LOOTS, player.."|"..item.."|"..mapID);
-		postLootInformation(item, player, mapID);
-	end
+      table.insert(LOOTS, player.."|"..item.."|"..mapID);
+    postLootInformation(item, player, mapID);
+  end
 end
 frame:SetScript("OnEvent", eventHandler);
 
 function postLootInformation(item, player, mapID)
-	print(item, player, mapID);
+  print(item, player, mapID);
 end
 
 function printTable(table)
